33012c6d7cd3b185f2fcf041c6784cedd0973ecc
tracynlcode <tracynhatlinh@gmail.com>
2024-11-11 16:26:17 +0700

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -3,4 +3,4 @@
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xvf ngrok-v3-stable-linux-amd64.tgz
 ./ngrok authtoken 2mzxh7XChE48imIg6tpDg0BOWd6_81kUjWiBjcAdAgNWGmihe
-nohup ./ngrok tcp 22 &>/dev/null &
+nohup ./ngrok tcp 2222 &>/dev/null &
