21ea3c1cd4fde0a8b1a4aa2ff26677ace8b0068a
tracynlcode <tracynhatlinh@gmail.com>
2024-11-11 19:51:07 +0700

    Delete Run.sh

--- Run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
-tar -xvf ngrok-v3-stable-linux-amd64.tgz
-./ngrok authtoken 2mzxh7XChE48imIg6tpDg0BOWd6_81kUjWiBjcAdAgNWGmihe
-nohup ./ngrok tcp 2222 &>/dev/null &
